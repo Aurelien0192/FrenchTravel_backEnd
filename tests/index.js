2f120f2a32cdff4a86c3327a180ccd12b8bc2b90
Aurelien0192 <mosini.aurelien@gmail.com>
2024-09-06 16:00:50 +0200

    ready to merge

--- tests/index.js
+++ tests/index.js
@@ -1,28 +1,28 @@
 require("../utils/database")
 
-// // describe("ApiLocationControllers",() => {
-// //    require('./controllers/ApiLocationController.test')
-// // })
-
-// describe("PlaceService", () => {
-//    require('./services/PlaceService.test')
+// describe("ApiLocationControllers",() => {
+//    require('./controllers/ApiLocationController.test')
 // })
 
-// describe("PlaceControllers", () => {
-//    require('./controllers/PlaceController.test')
-// })
+describe("PlaceService", () => {
+   require('./services/PlaceService.test')
+})
 
-// describe("UserService",() => {
-//    require('./services/UserService.test')
-// })
+describe("PlaceControllers", () => {
+   require('./controllers/PlaceController.test')
+})
 
-// describe("UserController",() => {
-//    require('./controllers/UserController.test')
-// })
+describe("UserService",() => {
+   require('./services/UserService.test')
+})
 
-// // describe("ApiLocationService", () => {
-// //    require('./services/ApiLocationService.test')
-// // })
+describe("UserController",() => {
+   require('./controllers/UserController.test')
+})
+
+// describe("ApiLocationService", () => {
+//    require('./services/ApiLocationService.test')
+// })
 
 // describe("ImageService",() => {
 //    require("./services/ImageService.test")
@@ -32,34 +32,34 @@ describe("ImageController",()=>{
    require("./controllers/ImageController.test")
 })
 
-// describe("CommentServices",() => {
-//    require("./services/CommentServices.test")
-// })
+describe("CommentServices",() => {
+   require("./services/CommentServices.test")
+})
 
-// describe("CommentController",() => {
-//    require("./controllers/CommentController.test")
-// })
+describe("CommentController",() => {
+   require("./controllers/CommentController.test")
+})
 
-// describe("LikeCommentService",() => {
-//    require("./services/LikeCommentService.test")
-// })
+describe("LikeCommentService",() => {
+   require("./services/LikeCommentService.test")
+})
 
-// describe("LikeCommentController",() => {
-//    require('./controllers/LikeCommentController.test')
-// })
+describe("LikeCommentController",() => {
+   require('./controllers/LikeCommentController.test')
+})
 
-// describe("FavoriteService",() => {
-//    require('./services/FavoriteService.test')
-// })
+describe("FavoriteService",() => {
+   require('./services/FavoriteService.test')
+})
 
-// describe("FavoriteController",()=>{
-//    require('./controllers/FavoriteController.test')
-// })
+describe("FavoriteController",()=>{
+   require('./controllers/FavoriteController.test')
+})
 
-// describe("FolderService",()=>{
-//    require('./services/FolderService.test')
-// })
+describe("FolderService",()=>{
+   require('./services/FolderService.test')
+})
 
-// describe("FolderController",()=>{
-//    require('./controllers/FolderController.test')
-// })
+describe("FolderController",()=>{
+   require('./controllers/FolderController.test')
+})
