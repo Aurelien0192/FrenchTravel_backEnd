fd2fb9b521b76d4e79587917ebecfc0521309425
Aurelien0192 <mosini.aurelien@gmail.com>
2024-09-09 15:11:25 +0200

    update: environment variable in database.js and docker-compose.yml

--- utils/database.js
+++ utils/database.js
@@ -7,7 +7,6 @@ mongoose.connection.on('open', () => logger.pino.info('connection open to the da
 mongoose.connection.on('disconnected', () => logger.pino.info('Disconnected to the database'))
 mongoose.connection.on('reconnected', () => logger.pino.info('Reconnected to the database'))
 mongoose.connection.on('close', () => logger.pino.info('connection to the database is close'))
-
-mongoose.connect(`mongodb://localhost:${process.env.PORT_BDD}/${process.env.npm_lifecycle_event === 'test'? "CDA_FRENCHTRAVEL_TEST":"CDA_FRENCHTRAVEL_PRODUCTION"}`)
+mongoose.connect(`${process.env.URL_DATABASE}:${process.env.PORT_BDD}/${process.env.npm_lifecycle_event === 'test'? "CDA_FRENCHTRAVEL_TEST":"CDA_FRENCHTRAVEL_PRODUCTION"}`)
 
 //CDA_FRENCHTRAVEL_PRODUCTION
